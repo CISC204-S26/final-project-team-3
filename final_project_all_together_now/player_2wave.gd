11d184e060533814e7a66945a2d15d8472724b7a
aedinmck <aedinmck@udel.edu>
2026-05-07 12:53:44 -0400

    both players working WITHOUT boundaries

---
+++ final_project_all_together_now/player_2wave.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+@export var move_speed = 250
+@export var nearby_interactables = []
+
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2.ZERO
+	var direction = Input.get_vector("wave_left", "wave_right", "wave_up", "wave_down")
+	if direction:
+		velocity= direction.normalized()*move_speed
+	#if Input.is_action_just_pressed("Interact"):
+		if nearby_interactables:
+			nearby_interactables.back().interact()
+	move_and_slide()
