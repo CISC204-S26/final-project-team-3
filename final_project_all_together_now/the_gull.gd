238213a59644a94b8c59c6acfe4e7c3e28d1d856
aedinmck <aedinmck@udel.edu>
2026-05-13 14:42:36 -0400

    gull2

--- final_project_all_together_now/the_gull.gd
+++ final_project_all_together_now/the_gull.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 var gull 
 var movement_speed = 250
-var movement_direction = Vector2(1,0.2)
+@export var movement_direction = Vector2(1,0.2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
